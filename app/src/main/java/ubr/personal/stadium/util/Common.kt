247f32aaf1599dc7dea2e8db04300de928b4c6ee
ubozorboyev <ulugbekbozorboyev97@gmail.com>
2021-06-15 20:02:20 +0500

    commit PK

--- app/src/main/java/ubr/personal/stadium/util/Common.kt
+++ app/src/main/java/ubr/personal/stadium/util/Common.kt
@@ -4,7 +4,7 @@ object Common {
 
     const val BASE_URL = "http://ulugbeyr.beget.tech/v1/"
 
-    const val IMAGE_URL =  "http://ulugbeyr.beget.tech/"
+    const val IMAGE_URL = "http://ulugbeyr.beget.tech/"
 
     var token = "ICKUR7Yv1QVurMqT1aG9amQDv-0iLesQ"
 
